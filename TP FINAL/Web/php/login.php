806d50ce594a495a986a21c7aa368e4264db9be6
saresq <santiago.aresq@gmail.com>
2015-11-06 16:07:04 -0300

    Validacion Login
    -Se cambio la version de jquery UI
    -Se añadio Jquery-min
    -Se valido el form Login
    -Se modifico el CSS para mostrar los errores
    -Se elimino un archivo temporal "~$abajo-practi...." que estaba demas

--- TP FINAL/Web/php/login.php
+++ TP FINAL/Web/php/login.php
@@ -3,7 +3,10 @@
 <?php include "rutas.php"; ?>
 	<head>
 	<meta charset="UTF-8">
-		<title>Sistema De Gestion Logistica</title>
+		<title>LOGIN | S.G.L</title>
+		
+		<script type="text/javascript" src="../js/funciones/jquery-1.11.3.min.js"></script>
+		<script type="text/javascript" src="../js/funciones/validarlogin.js"></script>
 		
 		<LINK REL="Stylesheet" HREF="../Css/login.css" TYPE="text/css">
 	</head>
@@ -14,26 +17,27 @@
 			<h1> Sistema de Gestión Logistica</h1>
 		</div>
 		
-		<form class='contacto' method="post" action="<?php echo $validar?>">
-			<div id="contacto">
+		<form id='login' method="post" action="<?php echo $validar?>">
 				
-				</br>
-				<div><label>LOGIN
 					</br>
-					<input type="text" name="usuario">
-					</label>
+				<div>
+					<label for="usuario">LOGIN</label></br>
+					<input name="usuario" type="text" id="usuario" class="usuario" placeholder="Ingrese usuario" autofocus=""/ >
+					<div id="mensaje1" class="errores"> Ingresa solo caracteres</div>
 				</div>
-				
-				</br>
-				
-				<div><label>PASSWORD
-				</br>
-					<input type="password" name="clave">
-					</label>
+					
+					
+
+				<div>
+					<label for="pass">PASSWORD</label></br>
+					<input name="pass" type="password" id="pass" class="pass" placeholder="Ingrese contraseña"/ >
+					<div id="mensaje2" class="errores"> Ingrese contraseña</div>
 				</div>
-				</br>
-				<input type="submit" value="Iniciar Sesion">
-			</div>
+
+					</br>
+
+				<input name="submit" type="submit" id="boton" value="Registrar" class="boton"/>
+			
 		</form>
 	</body>
 </html>
